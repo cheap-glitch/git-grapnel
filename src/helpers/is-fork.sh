f2c203fe9910b4f8b14cd2cfaebccdaa9e20cddb
cheap-glitch <cheap.glitch@gmail.com>
2020-09-27 23:16:47 +0200

    🐛 fix: Fix `is_fork()` not properly returning value

--- src/helpers/is-fork.sh
+++ src/helpers/is-fork.sh
@@ -1,4 +1,4 @@
 is_fork() {
 	local repo_url="https://api.github.com/repos/cheap-glitch/$(basename "$(git root)")"
-	if [[ "$(curl --silent "${repo_url}" | jq -r .fork)" == 'true' ]]; then return 1; else return 0; fi
+	if [[ "$(curl --silent "${repo_url}" | jq -r .fork)" == 'true' ]]; then echo 1; else echo 0; fi
 }
