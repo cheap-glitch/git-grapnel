c3c2d72fb14dd11e0a7ed7d06687364224c29f1e
cheap-glitch <cheap.glitch@gmail.com>
2020-09-27 20:08:55 +0200

    ♻️  refactor: Cache result of `is_repo()`

--- src/helpers/is-fork.sh
+++ src/helpers/is-fork.sh
@@ -1,3 +1,4 @@
 is_fork() {
-	[[ "$(curl --silent "https://api.github.com/repos/cheap-glitch/$(basename "$(git root)")" | jq -r .fork)" == 'true' ]]
+	local repo_url="https://api.github.com/repos/cheap-glitch/$(basename "$(git root)")"
+	if [[ "$(curl --silent "${repo_url}" | jq -r .fork)" == 'true' ]]; then return 1; else return 0; fi
 }
