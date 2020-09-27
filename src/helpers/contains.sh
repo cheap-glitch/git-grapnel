c777154b6e16dac9c62596b33f00b0e5c9f450c1
cheap-glitch <cheap.glitch@gmail.com>
2020-09-27 20:09:22 +0200

    ✨ style: Improve code style

--- src/helpers/contains.sh
+++ src/helpers/contains.sh
@@ -1,6 +1,5 @@
 contains() {
 	local list="$1"
 	local string="$2"
-
 	[[ "${list}" =~ (^|[[:space:]])"${string}"([[:space:]]|$) ]]
 }
--- src/helpers/is-changed.sh
+++ src/helpers/is-changed.sh
@@ -1,5 +1,4 @@
 is_changed() {
 	local file="$1"
-
 	git diff --name-only 'HEAD@{2}' HEAD | grep -Fq "^${file}$"
 }
