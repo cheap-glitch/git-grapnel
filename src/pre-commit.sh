dc4f54c0cf55ecbc4470a072dc1a697314dd61a4
cheap-glitch <cheap.glitch@gmail.com>
2020-11-28 11:25:22 +0100

    🐛 fix: Only perform pre-commit checks on source repos

--- src/pre-commit.sh
+++ src/pre-commit.sh
@@ -2,6 +2,10 @@
 set -eu -o pipefail
 IFS=$'\n\t'
 
+DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+# shellcheck source=src/helpers/is-fork.sh
+source "${DIR}/helpers/is-fork.sh"
+
 EXCLUDE_FROM_COMMITS='
 .codecov.yml
 .npmrc
@@ -12,13 +16,15 @@ node_modules
 test-app
 '
 
-# Ensure the repo has a gitignore
-if [[ ! -f .gitignore ]]; then echo "Need a .gitignore!"; exit 1; fi
+if ! is_fork; then
+	# Ensure the repo has a gitignore
+	if [[ ! -f .gitignore ]]; then echo "Need a .gitignore!"; exit 1; fi
 
-# Ensure that some dangerous files and folders are not committed
-for exclude in ${EXCLUDE_FROM_COMMITS}; do
-	if rg -Fq "${exclude}" .gitignore; then continue; fi
+	# Ensure that some dangerous files and folders are not committed
+	for exclude in ${EXCLUDE_FROM_COMMITS}; do
+		if rg -Fq "${exclude}" .gitignore; then continue; fi
 
-	if [[ -f "${exclude}" ]]; then echo   "File '${exclude}' should be added to .gitignore!"; exit 1; fi
-	if [[ -d "${exclude}" ]]; then echo "Folder '${exclude}' should be added to .gitignore!"; exit 1; fi
-done
+		if [[ -f "${exclude}" ]]; then echo   "File '${exclude}' should be added to .gitignore!"; exit 1; fi
+		if [[ -d "${exclude}" ]]; then echo "Folder '${exclude}' should be added to .gitignore!"; exit 1; fi
+	done
+fi
