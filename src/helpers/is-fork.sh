38ae828a48d332bce6e436b56eb74e2cd9ba1604
cheap-glitch <cheap.glitch@gmail.com>
2020-11-28 11:34:02 +0100

    🐛 fix: Use a better key when caching repos fork status

--- src/helpers/is-fork.sh
+++ src/helpers/is-fork.sh
@@ -5,7 +5,10 @@ source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/cache.sh"
 
 is_fork() {
 	local repo_url
-	repo_url="https://api.github.com/repos/$(git config --get user.name)/$(basename "$(pwd)")"
+	local repo_name
 
-	[[ "$(cache "isRepo.\"${repo_url}\"" "curl --silent ${repo_url} | jq --raw-output '.fork // false'")" == 'true' ]]
+	repo_name="$(basename "$(pwd)")"
+	repo_url="https://api.github.com/repos/$(git config --get user.name)/${repo_name}"
+
+	[[ "$(cache "isFork.\"${repo_name}\"" "curl --silent ${repo_url} | jq --raw-output '.fork // false'")" == 'true' ]]
 }
