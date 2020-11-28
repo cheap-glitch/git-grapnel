2d56f992f508216c8cb4c515c77e8391c4fe2886
cheap-glitch <cheap.glitch@gmail.com>
2020-11-28 11:26:24 +0100

    🐛 fix: Avoid fetching fork status every time for non-GitHub repos

--- src/helpers/is-fork.sh
+++ src/helpers/is-fork.sh
@@ -7,5 +7,5 @@ is_fork() {
 	local repo_url
 	repo_url="https://api.github.com/repos/$(git config --get user.name)/$(basename "$(pwd)")"
 
-	[[ "$(cache "isRepo.\"${repo_url}\"" "curl --silent ${repo_url} | jq -r .fork")" == 'true' ]]
+	[[ "$(cache "isRepo.\"${repo_url}\"" "curl --silent ${repo_url} | jq --raw-output '.fork // false'")" == 'true' ]]
 }
