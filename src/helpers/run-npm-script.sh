7ead7b85a7851eefe7e4e6f31e40a6b6ca728e78
cheap-glitch <cheap.glitch@gmail.com>
2021-12-27 21:38:24 +0100

    Lint

--- src/helpers/run-npm-script.sh
+++ src/helpers/run-npm-script.sh
@@ -1,8 +1,8 @@
 # shellcheck shell=bash
 
-run_npm_script() {
+run_first_available_npm_script() {
 	for script in "$@"; do
-		if jq --exit-status ".scripts.\"${script}\"" package.json &> /dev/null; then
+		if jq --exit-status ".scripts.\"${script}\"" package.json &>/dev/null; then
 			npm run "${script}"
 			return
 		fi
--- src/post-merge.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu -o pipefail
-IFS=$'\n\t'
-
-DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-# shellcheck source=src/helpers/is-changed.sh
-source "${DIR}/helpers/is-changed.sh"
-
-if [[ -f package-lock.json ]] && is_changed 'package-lock.json'; then npm clean-install; fi
