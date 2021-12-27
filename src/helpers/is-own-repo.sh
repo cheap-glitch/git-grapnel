2a5b98c70520c9f5e7c462669946a2d4f83e588d
cheap-glitch <cheap.glitch@gmail.com>
2021-12-27 21:35:27 +0100

    Fix and simplify detection of owned repos

---
+++ src/helpers/is-own-repo.sh
@@ -0,0 +1,8 @@
+# shellcheck shell=bash
+
+is_own_repo() {
+	# Check that the local repo is not a GitHub fork (no `upstream` branch)
+	# and is not owned by someone else (the `origin` URL contains the user's name)
+	[[ -z "$(git remote)" ]] || \
+	(! git remote get-url upstream &>/dev/null && git remote get-url origin 2>/dev/null | rg --quiet --fixed-strings "$(git config user.name)/")
+}
