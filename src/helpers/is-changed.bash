6874be5afde4eacd25dd858ae35da4ef9a790740
cheap-glitch <cheap.glitch@gmail.com>
2022-04-09 09:58:07 +0200

    Fix detection of changed files in `post-merge` hook

--- src/helpers/is-changed.bash
+++ src/helpers/is-changed.bash
@@ -2,6 +2,8 @@ is_changed() {
 	mapfile -t changed_files < <(git diff --name-only 'HEAD@{2}..')
 
 	for file in "${changed_files[@]}"; do
-		if [[ "${file}" == "${1}" ]]; then return 1; fi
+		if [[ "$(basename "${file}")" == "${1}" ]]; then return 0; fi
 	done
+
+	return 1
 }
